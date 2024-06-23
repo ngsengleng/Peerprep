c4723819382c4c98ef87f6952c3e821a257be8c7
ngsengleng <ngsengleng@gmail.com>
2024-06-24 00:39:18 +0800

    change dir name

--- auth-server/genProto.sh
+++ auth-server/genProto.sh
@@ -1,7 +1,8 @@
 cd ../pbDefinitions
 
-eval "protoc --go_out=../auth-server/pb --go_opt=paths=source_relative \
-    --go-grpc_out=../auth-server/pb --go-grpc_opt=paths=source_relative \
+DEST="../auth-server/tokenverifier"
+eval "protoc --go_out=$DEST --go_opt=paths=source_relative \
+    --go-grpc_out=$DEST --go-grpc_opt=paths=source_relative \
     tokenverifier.proto"
 
 cd ../auth-server && go mod tidy
